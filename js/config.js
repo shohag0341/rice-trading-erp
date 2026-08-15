d2a1fd2ca52a442fa3f200945d68f8eb6c0ce6ea
shohag0341 <shohag26357@gmail.com>
2026-08-15 14:08:47 +0600

    Remove KG_PER_MAUND constant from config.js
    Removed the business constant KG_PER_MAUND from config.

--- js/config.js
+++ js/config.js
@@ -4,8 +4,5 @@
 export const SUPABASE_URL = "https://bvxtpdnonmrwdnlohbgs.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_NbLwJ4VU5oC-M7SJv-oQaw_C3LIgZle";
 
-// Business constants
-export const KG_PER_MAUND = 37.32;
-
 // App info
 export const APP_NAME = "Rice Trading ERP Pro";
